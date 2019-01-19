60fc6d2ccf8b2c427993ac885ab5b92fc112e4c5
EmmaRocheteau <ecr38@cam.ac.uk>
2019-01-19 14:29:40 +0000

    read and writing video working

--- get_frames.py
+++ get_frames.py
@@ -1,48 +1,29 @@
-import cv2
-import time
-import os
+import skvideo.io
+import skvideo.datasets
 
-def video_to_frames(input_loc, output_loc):
-    """Function to extract frames from input video file
-    and save them as separate frames in an output directory.
-    Args:
-        input_loc: Input video file.
-        output_loc: Output directory to save the frames.
-    Returns:
-        None
+def video_to_frames(input_loc):
     """
-    try:
-        os.mkdir(output_loc)
-    except OSError:
-        pass
-    # Log the time
-    time_start = time.time()
-    # Start capturing the feed
-    cap = cv2.VideoCapture(input_loc)
-    # Find the number of frames
-    video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
-    print ("Number of frames: ", video_length)
-    count = 0
-    print ("Converting video..\n")
-    # Start converting the video
-    while cap.isOpened():
-        # Extract the frame
-        ret, frame = cap.read()
-        # Write the results back to output location.
-        cv2.imwrite(output_loc + "/%#05d.jpg" % (count+1), frame)
-        count = count + 1
-        # If there are no more frames left
-        if (count > (video_length-1)):
-            # Log the time again
-            time_end = time.time()
-            # Release the feed
-            cap.release()
-            # Print stats
-            print ("Done extracting frames.\n%d frames extracted" % count)
-            print ("It took %d seconds forconversion." % (time_end-time_start))
-            break
+    Deconstructs video to frames
+
+    :param input_loc: Video file
+    :return: numpy array with dimensions (time, height, width, channels)
+    """
+    return skvideo.io.vread(input_loc)
+
+
+def frames_to_video(array_of_frames, output_loc):
+    """
+    Reconstructs video from frames, assuming
+    :param array_of_frames:
+    :param output_loc:
+    :return:
+    """
+    return skvideo.io.vwrite(output_loc, array_of_frames)
+
 
 if __name__=='__main__':
     input_loc = './test_videos/IMG_0037.m4v'
-    output_loc = './extracted_frames/'
-    video_to_frames(input_loc, output_loc)
+    output_loc = './test_videos/reconstructed.mp4'
+    array_of_frames = video_to_frames(input_loc)
+    frames_to_video(array_of_frames, output_loc)
+
