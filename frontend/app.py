e718dd2a1df260ea44956c1db44335382b354f5b
Petar Veličković <pv273@cam.ac.uk>
2019-01-19 22:51:01 +0000

    Proto enhans

--- frontend/app.py
+++ frontend/app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, render_template, request, send_from_directory
 from os import listdir
 from os.path import isfile, join
+from backend.get_frames import video_to_frames, frames_to_video
+from backend.nhancer import nhance
 
 app = Flask(__name__)
 
@@ -14,11 +16,12 @@ def index():
 
 @app.route('/upload', methods=["POST"])
 def upload():
-    target = os.path.join(APP_ROOT, 'videos')
+    target = os.path.join(APP_ROOT, 'originals')
     file = request.files.getlist("file")[0]
     filename = file.filename
     destination = "/".join([target, filename])
     file.save(destination)
+    set_original(destination)
 
     return index()
 
@@ -26,16 +29,41 @@ def upload():
 def get_video(path):
     return send_from_directory('videos', path)
 
+@app.route('/getorig/<path:path>')
+def get_original(path):
+    return send_from_directory('originals', path)
+
+@app.route('/getenh/<path:path>')
+def get_enhanced(path):
+    return send_from_directory('enhanced', path)
+
+@app.route('/getsplit/<path:path>')
+def get_split(path):
+    return send_from_directory('split', path)
+
+@app.route('setorig/<path:path>')
+def set_original(path):
+    file = send_from_directory('originals', path)
+    target = os.path.join(APP_ROOT, 'videos')
+    filename = file.filename
+    destination = "/".join([target, filename])
+    file.save(destination)
+
+    return index()
+
+@app.route('setenh/<path:path>')
+def set_enhanced(path):
+    file = send_from_directory('enhanced', path)
+    target = os.path.join(APP_ROOT, 'videos')
+    filename = file.filename
+    destination = "/".join([target, filename])
+    file.save(destination)
+
+    return index()
+
 @app.route('/video/<id>')
 def video(id):
     return render_template('video.html', id=id)
-
-@app.route('/enhance/<id>')
-def enhance(id):
-    # Run the code to enhance the video
-    return video(id)
-
-
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
